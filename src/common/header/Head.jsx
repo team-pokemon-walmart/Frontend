3a91777ae03e59b496ef26f36b5f6db961971833
Aman <krishanaman14@gmail.com>
2023-08-11 15:43:37 +0530

    first

---
+++ src/common/header/Head.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+
+const Head = () => {
+  return (
+    <>
+      <section className='head'>
+        <div className='container d_flex'>
+          <div className='left row'>
+            <i className='fa fa-phone'></i>
+            <label> 9845405676</label>
+            <i className='fa fa-envelope'></i>
+            <label> support@ui-lib.com</label>
+          </div>
+          <div className='right row RText'>
+            <label>Theme FAQ"s</label>
+            <label>Need Help?</label>
+            <span>🏳️‍⚧️</span>
+            <span> 🇮🇳</span>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Head
